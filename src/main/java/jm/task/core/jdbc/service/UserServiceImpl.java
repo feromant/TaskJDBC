3ba25b89cee396a50ef251ca2f8e3e6f9abdd2f0
feromant <maznichenko@list.ru>
2021-09-23 20:06:13 +0300

    correct Service

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,106 +1,35 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    static {
-        Util.initDriver();
-    }
-    private static long userCount = 1;
+    private final UserDao dao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-
-            String query = "CREATE TABLE IF NOT EXISTS users " +
-                    "(id BIGINT UNIQUE AUTO_INCREMENT PRIMARY KEY, " +
-                    "name VARCHAR(10), " +
-                    "lastname VARCHAR(10), " +
-                    "age TINYINT)";
-            statement.executeUpdate(query);
-        } catch (SQLException ex) {
-            System.out.println("SQLException: " + ex.getMessage());
-        }
+        dao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-
-            statement.executeUpdate("DROP TABLE IF EXISTS users");
-        } catch (SQLException ex) {
-            System.out.println("SQLException: " + ex.getMessage());
-        }
+        dao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
-        String query = "INSERT INTO users (id, name, lastname, age) VALUES(?, ?, ?, ?)";
-
-        try (Connection connection = Util.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-
-            statement.setLong(1, userCount);
-            statement.setString(2, name);
-            statement.setString(3, lastName);
-            statement.setByte(4, age);
-
-            statement.executeUpdate();
-            System.out.println("User с именем " + name + " добавлен в базу данных");
-            userCount++;
-        } catch (SQLException ex) {
-            System.out.println("SQLException: " + ex.getMessage());
-        }
+        dao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
-        String query = "DELETE FROM users WHERE id = ?";
-
-        try (Connection connection = Util.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setLong(1, id);
-            statement.executeUpdate();
-        } catch (SQLException ex) {
-            System.out.println("SQLException: " + ex.getMessage());
-        }
+        dao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-
-        List<User> users = new ArrayList<>();
-
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT * FROM users")) {
-
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastname"));
-                user.setAge(resultSet.getByte("age"));
-                System.out.println(user);
-                users.add(user);
-            }
-        } catch (SQLException ex) {
-            System.out.println("SQLException: " + ex.getMessage());
-        }
-        return users;
+        return dao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-
-            statement.executeUpdate("DELETE FROM users");
-        } catch (SQLException ex) {
-            System.out.println("SQLException: " + ex.getMessage());
-        }
+        dao.cleanUsersTable();
     }
 }
